53ab9b4ee6325864dbcd77504132dc291ae0a995
aikoreainc <aikoreainc@gmail.com>
2026-05-14 13:09:32 +0900

    feat: 협력기관 로고 5종 + 명단 8곳으로 확장
    BISTEP, SMC, KRIVET, KTL, NIWDC 로고 이미지 추가.
    partners.ts에 KRIVET·KTL·NIWDC 신규 항목 추가.
    PartnersStrip이 로고 유무에 따라 자동 분기.

--- src/data/partners.ts
+++ src/data/partners.ts
@@ -15,6 +15,7 @@ export const partners: Partner[] = [
     slug: "smc",
     name: { ko: "삼성서울병원", en: "Samsung Medical Center" },
     url: "https://www.samsunghospital.com",
+    logo: "/img/partners/smc.png",
   },
   {
     slug: "skku",
@@ -29,5 +30,24 @@ export const partners: Partner[] = [
     slug: "bistep",
     name: { ko: "BISTEP", en: "BISTEP" },
     url: "https://www.bistep.re.kr",
+    logo: "/img/partners/bistep.png",
+  },
+  {
+    slug: "krivet",
+    name: { ko: "한국직업능력연구원", en: "Korea Research Institute for Vocational Education and Training" },
+    url: "https://www.krivet.re.kr",
+    logo: "/img/partners/krivet.png",
+  },
+  {
+    slug: "ktl",
+    name: { ko: "한국산업기술시험원", en: "Korea Testing Laboratory" },
+    url: "https://www.ktl.re.kr",
+    logo: "/img/partners/ktl.svg",
+  },
+  {
+    slug: "niwdc",
+    name: { ko: "국립야생동물질병관리원", en: "National Institute of Wildlife Disease Control" },
+    url: "https://www.me.go.kr/niwdc",
+    logo: "/img/partners/niwdc.jpg",
   },
 ];
